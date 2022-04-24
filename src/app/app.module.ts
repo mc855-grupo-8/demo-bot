730aea02299cdb5e8f3b45fda2d43042905ea945
Pedro Bastos <pedro.bbastos123@gmail.com>
2022-04-24 15:27:44 -0300

    Added firebase to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,24 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { NbThemeModule, NbLayoutModule, NbChatModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBFu6jeght-0DKzShzlvoLhlDNyofuS7_s",
+  authDomain: "fir-bot-870ad.firebaseapp.com",
+  projectId: "fir-bot-870ad",
+  storageBucket: "fir-bot-870ad.appspot.com",
+  messagingSenderId: "735965424491",
+  appId: "1:735965424491:web:80e19c9135faeab2c5ba2e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 @NgModule({
   declarations: [
     AppComponent
