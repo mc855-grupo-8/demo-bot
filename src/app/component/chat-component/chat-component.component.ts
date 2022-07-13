99396ad177dfd9545a1e303d025d933970e82fa1
João Guilherme <joaog.oliveira96@gmail.com>
2022-07-13 17:41:13 -0300

    issues #15 #16 e #24

--- src/app/component/chat-component/chat-component.component.ts
+++ src/app/component/chat-component/chat-component.component.ts
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
 export class ChatComponentComponent {
   botUser: any = {
     name: 'Bot',
-    avatar: 'https://i.gifer.com/no.gif',
+    avatar: 'https://cdn-icons-png.flaticon.com/512/204/204245.png',
   };
 
   visitanteUser: any = {
@@ -27,11 +27,10 @@ export class ChatComponentComponent {
     introduction: {
       ...this.model,
       text:
-        'Seja bem-vindo ao Ambulatório da pediatria, poderia informar sobre você?\n\n' +
-        'Digite um dos números abaixo.\n' +
-        '1. Sou paciente e é minha primeira vez no HC\n' +
-        '2. Sou paciente cadastrado\n' +
-        '3. Estou levando um paciente',
+        'Seja bem-vindo ao Ambulatório da Pediatria. Digite uma das opções abaixo para continuar:\n\n' +
+        '1. Sou paciente e esta é a minha primeira vez no Hospital das Clínicas.\n' +
+        '2. Sou paciente e já possuo cadastrado.\n' +
+        '3. Estou levando um paciente.',
       options: [
         'im-pacient-non-registered',
         'im-pacient-registered',
@@ -41,21 +40,21 @@ export class ChatComponentComponent {
     'im-pacient-non-registered': {
       ...this.model,
       text:
-        'Pacientes não registrados precisam passar na faixa amarela para realizar o seu cadastro antes de fazer sua consulta na pediatria.\n' +
+        'Pacientes não registrados precisam passar na faixa amarela para realizar o seu cadastro antes de fazer sua consulta na pediatria.\n\n' +
         'Certifique-se que trouxe um comprovante de identidade com foto, comprovante de endereço e tenha um email válido.\n\n' +
-        'Uma vez feito o cadastro, qual a razão da visita ao Ambulatório?\n\n' +
-        '1. Consulta \n' +
-        '2. Preciso remarcar consulta, pegar um relatório ou tirar uma receita\n' +
-        '3. Preciso de um medicamento, teste, curativo ou outro procedimento\n',
+        'Uma vez feito o cadastro, qual a razão da visita ao ambulatório?\n\n' +
+        '1. Consulta.\n' +
+        '2. Preciso remarcar uma consulta, pegar um relatório ou tirar uma receita.\n' +
+        '3. Preciso de um medicamento, teste, curativo ou outro procedimento.\n',
       options: ['exam-type', 'talk-to-reception', 'wait'],
     },
     'im-pacient-registered': {
       ...this.model,
       text:
         'Qual o motivo da visita?\n\n' +
-        '1. Consulta \n' +
-        '2. Remarcação de consulta, relatório ou receita\n' +
-        '3. Preciso de medicamento, teste ou curativo\n',
+        '1. Consulta.\n' +
+        '2. Remarcação de consulta, relatório ou receita.\n' +
+        '3. Preciso de medicamento, teste ou curativo.\n',
       options: ['exam-type', 'talk-to-reception', 'wait'],
     },
     'talk-to-reception': {
@@ -65,24 +64,24 @@ export class ChatComponentComponent {
     },
     wait: {
       ...this.model,
-      text: 'Por favor, reserve seu HC, que é o número dado pela recepção e em breve você será atendido/a.',
+      text: 'Por favor, reserve seu HC, que é o número dado pela recepção e em breve você será atendido.',
       options: ['finish'],
     },
     'im-taking-pacient': {
       ...this.model,
       text:
-        'É a primeira visita do paciente ?\n\n' +
-        '1. Sim, é a primeira visita do paciente para consulta\n' +
-        '2. Não, o paciente já realizou uma ou mais consultas antes\n',
+        'É a primeira visita do paciente?\n\n' +
+        '1. Sim, é a primeira visita do paciente para consulta.\n' +
+        '2. Não, o paciente já realizou uma ou mais consultas antes.\n',
       options: ['im-pacient-non-registered', 'im-pacient-registered'],
     },
     'exam-type': {
       ...this.model,
       text:
         'Sua consulta foi agendada?\n\n' +
-        '1. Sim, agendei minha consulta e recebi pelo email \n' +
-        '2. Não, minha consulta é um encaixe por email\n' +
-        '3. Não agendei minha consulta e não recebi email\n',
+        '1. Sim, agendei minha consulta e recebi pelo email.\n' +
+        '2. Não, minha consulta é um encaixe por email.\n' +
+        '3. Não agendei minha consulta e não recebi email.\n',
       options: [
         'scheduled-exam',
         'not-scheduled-exam-by-email',
@@ -91,23 +90,27 @@ export class ChatComponentComponent {
     },
     'scheduled-exam': {
       ...this.model,
-      text: 'Retire sua senha física e aguarde para ser atendido/a. Por favor, certifique-se que seu documento de identidade com foto e o email usado no agendamento estão com você.\n',
+      text: 'Retire sua senha física e aguarde para ser atendido. Por favor, certifique-se que seu documento de identidade com foto e o email usado no agendamento estão com você.\n',
       options: ['finish'],
     },
     'not-scheduled-exam-by-email': {
       ...this.model,
-      text: 'Dirija-se à recepção da faixa amarela para registrar sua consulta no sistema. Uma vez finalizado, dirija-se à faixa cinza para ser atendido\n',
+      text: 'Dirija-se à recepção da faixa amarela para registrar sua consulta no sistema. Uma vez finalizado, dirija-se à faixa cinza para ser atendido.\n',
       options: ['finish'],
     },
     'not-scheduled-exam': {
       ...this.model,
-      text: 'Entre em contato com a recepção para que possa ser atendido o mais rápido possível\n',
+      text: 'Entre em contato com a recepção para que possa ser atendido o mais rápido possível.\n',
       options: ['finish'],
     },
-    finish: {
+    'finish': {
       ...this.model,
-      text: 'Agradecemos por usar o nosso chatbot!\n\nCaso não tenha encontrado sua dúvida, consulte nosso FAQ na página inicial ou digite 1 para começar a conversa novamente\n',
-      options: ['introduction'],
+      text: 'Agradecemos por usar o nosso chatbot!\n\nCaso não tenha encontrado sua dúvida, consulte nosso FAQ na página inicial.',
+      options: ['feedback'],
+    },
+    'feedback': {
+      ...this.model,
+      text: 'Nós amamos ouvir os nossos pacientes, nos envie uma sugestão ou crítica através do link abaixo.',
     },
   };
 
@@ -124,6 +127,7 @@ export class ChatComponentComponent {
         this.botUser,
         this.messageList[optionValue]?.options
       );
+
       this.lastMessage = this.messages.slice(-1)[0];
       this.sendNextOption(reply);
     } else {
@@ -133,15 +137,28 @@ export class ChatComponentComponent {
 
   sendNextOption(reply: string) {
     const optionValue: any = this.lastMessage?.options[parseInt(reply) - 1];
+
     if (this.lastMessage?.options[0] == 'finish') {
       setTimeout(() => {
         this.pushMessage(
           this.messageList['finish'].text,
           false,
           this.botUser,
-          this.messageList['finish']?.options
+          this.messageList['finish']?.options,
         );
+        
         this.lastMessage = this.messages.slice(-1)[0];
+
+        setTimeout(() => {
+          this.pushMessage(
+            this.messageList['feedback'].text,
+            false,
+            this.botUser,
+            this.messageList['feedback']?.options,
+            "link"
+          );
+          this.lastMessage = this.messages.slice(-1)[0];
+        }, 4000);
       }, 4000);
     }
   }
@@ -158,12 +175,13 @@ export class ChatComponentComponent {
     message: string,
     isReply: boolean,
     iconModel: any,
-    options?: any
+    options?: any,
+    type?: string,
   ) {
     this.messages.push({
       text: message,
       date: new Date(),
-      type: 'text',
+      type: type ?? 'text',
       reply: isReply,
       options: options,
       user: iconModel,
